fd10a1a90c9a26f734efeb4a2697c97022730a14
Dedas <andreas.winkler@winkit.se>
2020-07-05 12:22:04 +0200

    minor hamburger menu tweak

--- src/components/theme/Header/Hamburger/styles.js
+++ src/components/theme/Header/Hamburger/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   z-index: 5;
-  top: 1.5rem;
+  top: 1.3rem;
   right: 1.8rem;
   display: none;
   cursor: pointer;
@@ -18,7 +18,7 @@ export const Wrapper = styled.div`
     sidebar &&
     `
 			right: 18%;
-			top: 1.5rem;
+			top: 1.3rem;
 		
 			@media (max-width: 960px) {
 				right: 1.8rem;
@@ -32,7 +32,7 @@ export const Wrapper = styled.div`
 
 export const Bar = styled.div`
 	width: 1.6rem;
-	height: .14rem;
+	height: .13rem;
 	margin-bottom: .3rem;
 	background-color: #212121;
 	transition: transform 500ms cubic-bezier(0.6, 0.05, 0.28, 0.91),
